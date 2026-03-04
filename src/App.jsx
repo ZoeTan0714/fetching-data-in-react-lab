4d77e978d9593442737d9cc1f59819390b1fa799
Zoe Tan <zoe.txy@outlook.com>
2026-03-05 00:48:14 +0800

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,26 @@ const App = () => {
       getAllStarships();    
   },[]);
 
-  
+  async function postData() {
+	  const url = "https://api.airtable.com/v0/appodGKgM3CqEfx4u/Table%201?maxRecords=3";
+	  try {
+	      const response = await fetch(url);
+        // headers: {
+        //   Authorization: "Bearer" + import.meta.env.VITE_Airtable_TOKEN
+        // } 
 
-  return (
-    <h1>Hello world!</h1>
-  );
-}
+        if (!response.ok) {
+	      throw new Error(`Response status: ${response.status}`);
+	    }
+
+	    const result = await response.json();
+	    return result;
+        
+	  } catch (error) {
+	    console.error(error);
+      throw new Error('Failed to fetch starships.');
+	  }
+	}
+};
 
 export default App
